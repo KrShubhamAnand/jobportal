40bb4367d3f66ba18ae006c61ddfbf582660c0e9
kumar shubham anand <kumarshubham.ks52@gmail.com>
2024-09-22 12:45:49 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobportal1-vj5s.onrender.com',
     credentials:true
 }
 
